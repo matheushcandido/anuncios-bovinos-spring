bca10e8be214cd5d525680542bc58967f8727cb2
matheushcandido <candidoma050@gmail.com>
2024-05-02 18:07:50 -0300

    feito endpoint pra validar codigo recebido.

--- src/main/java/insetec/backend/controllers/SmsController.java
+++ src/main/java/insetec/backend/controllers/SmsController.java
@@ -1,13 +1,14 @@
 package insetec.backend.controllers;
 
+import insetec.backend.models.User;
+import insetec.backend.repositories.UserRepository;
 import insetec.backend.services.SmsService;
 import insetec.backend.models.Sms;
 import insetec.backend.repositories.SmsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/sms")
@@ -19,6 +20,9 @@ public class SmsController {
     @Autowired
     private SmsRepository smsRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/send")
     public void sendSms(@RequestBody Sms sms) {
 
@@ -26,5 +30,25 @@ public class SmsController {
 
         String messageBody = "Seu código de verificação: " + sms.getCode();
         smsService.sendSms(sms.getPhoneNumber(), messageBody);
+    }
+
+    @PostMapping("/validate")
+    public boolean validateSms(@RequestParam String userId, @RequestParam String code) {
+        Sms sms = smsRepository.findByUserId(userId);
+        Optional<User> userOptional = userRepository.findById(userId);
+
+        if (sms != null && userOptional.isPresent()) {
+            User user = userOptional.get();
+
+            if (sms.getCode().equals(code)) {
+                user.getContact().setVerified(true);
+                userRepository.save(user);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 }
--- src/main/java/insetec/backend/repositories/SmsRepository.java
+++ src/main/java/insetec/backend/repositories/SmsRepository.java
@@ -2,7 +2,12 @@ package insetec.backend.repositories;
 
 import insetec.backend.models.Sms;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SmsRepository extends JpaRepository<Sms, String> {
+
+    @Query("SELECT * FROM verification-codes WHERE user_id = :userId")
+    Sms findByUserId(@Param("userId") String userId);
 
 }
